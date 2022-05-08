8608bb0f72e4ab30e9d7a27844785e78d5fd7398
James-VT <jd_tanner@hotmail.co.uk>
2022-05-09 00:07:54 +0100

    Adds variables for cooking level choices

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
 let beefCut = document.getElementById("beef-cut");
 
 let beefCutChoice = beefCut.options[beefCut.selectedIndex].value;
+
+// Set variables for weight input here, above cooking choices
+
+let howCooked = document.getElementById("how-cooked");
+
+let howCookedChoice = howCooked.options[howCooked.selectedIndex].value;
