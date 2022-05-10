19619a03706eddb8cd23ebfa00e5c0a47f9e7ac5
James-VT <jd_tanner@hotmail.co.uk>
2022-05-10 11:32:59 +0100

    Adds more work to the JS with more variables and the first couple of functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,23 @@ let beefCutChoice = beefCut.options[beefCut.selectedIndex].value;
 let howCooked = document.getElementById("how-cooked");
 
 let howCookedChoice = howCooked.options[howCooked.selectedIndex].value;
+
+const calculateButton = document.getElementById("calculate-button");
+
+// Event listener for the calculation
+
+calculateButton.addEventListener("click", calculate);
+
+// Change which option has the select tag in the html
+
+function updateCutChoice() {
+    let beefCutChoice = beefCut.options[beefCut.selectedIndex].value;
+    console.log(beefCutChoice);
+}
+
+// Calculate function
+
+function calculate() {
+    console.log(beefCutChoice);
+    console.log(howCookedChoice);
+}
