14e97d13dab8c84a379a673207cb1e13dab0bdf1
James-VT <jd_tanner@hotmail.co.uk>
2022-05-10 23:29:26 +0100

    Includes JS missing from previous commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,5 @@
 let beefCut = document.getElementById("beef-cut");
 
-let beefCutChoice = ""
-
 // Set variables for weight input here, above cooking choices
 
 let howCooked = document.getElementById("how-cooked");
@@ -14,18 +12,10 @@ const calculateButton = document.getElementById("calculate-button");
 
 calculateButton.addEventListener("click", calculate);
 
-// Change which option has the select tag in the html
-
-function updateCutChoice() {
-    var beefCutChoice = beefCut.options[beefCut.selectedIndex].value;
-    console.log(beefCutChoice);
-    return beefCutChoice;
-}
-
 // Calculate function
 
 function calculate() {
-    updateCutChoice();
+    let beefCutChoice = beefCut.options[beefCut.selectedIndex].value;
     console.log(beefCutChoice);
     console.log(howCookedChoice);
 }
