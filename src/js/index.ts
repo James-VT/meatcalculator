b7a0b8d8702235d2c701dc16f3a41a0defb41bb5
Jams-VT <jd_tanner@hotmail.co.uk>
2023-06-18 14:10:19 +0100

    Converts js to ts, and types HTML elements appropriately

---
+++ src/js/index.ts
@@ -0,0 +1,34 @@
+let beefCut = (document.getElementById("beef-cut")) as HTMLSelectElement;
+
+// Set variables for weight input here, above cooking choices
+
+let cutWeight = (document.getElementById("cut-weight")) as HTMLInputElement;
+
+let howCooked = (document.getElementById("how-cooked")) as HTMLSelectElement;
+
+const calculateButton = document.getElementById("calculate-button");
+
+// Event listener for the calculation
+
+calculateButton?.addEventListener("click", calculate);
+
+// Calculate function (these are all just testers for now)
+
+function calculate() {
+    let beefCutChoice = beefCut?.options[beefCut.selectedIndex].value;
+    let howCookedChoice = howCooked?.options[howCooked.selectedIndex].value;
+    let cutWeightChoice = cutWeight?.value;
+    console.log(beefCutChoice);
+    console.log(cutWeightChoice);
+    console.log(howCookedChoice);
+};
+
+function beefCutSelection() {
+    let howCookedSelected = howCooked?.options[howCooked.selectedIndex].value;
+
+    if (howCookedSelected === "rare") {
+        console.log("rare chosen")
+    } else {
+        console.log("not rare")
+    }
+};
