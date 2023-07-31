c052c0ce0046f44bf21d7264617946eb0d026af0
Jams-VT <jd_tanner@hotmail.co.uk>
2023-07-31 20:15:49 +0100

    Refactors additional function

--- src/js/index.ts
+++ src/js/index.ts
@@ -12,24 +12,35 @@ const calculateButton = document.getElementById("calculate-button");
 
 calculateButton?.addEventListener("click", calculate);
 
-function beefCutSelection() {
-    let howCookedSelected = howCooked?.options[howCooked.selectedIndex].value;
-
-    if (howCookedSelected === "rare") {
-        console.log("rare chosen")
-    } else {
-        console.log("not rare")
-    }
-};
-
 // Calculate function (these are all just testers for now)
 
+function beefCutSelection() {
+    console.log("function called");
+    let howCookedSelected = howCooked?.options[howCooked.selectedIndex].value;
+    console.log(howCookedSelected);
+
+    if (howCookedSelected === "rare") {
+        const beefCutString = "rare chosen";
+        return console.log(beefCutString);
+    } else if (howCookedSelected != "rare") {
+        const beefCutString = "not rare";
+        return console.log(beefCutString);
+    } else {
+        return console.log("nothing doing");
+    };
+
+    //return beefCutString
+};
+
 function calculate() {
+    beefCutSelection();
     let beefCutChoice = beefCut?.options[beefCut.selectedIndex].value;
     let howCookedChoice = howCooked?.options[howCooked.selectedIndex].value;
     let cutWeightChoice = cutWeight?.value;
-    beefCutSelection();
+
     console.log(beefCutChoice);
     console.log(cutWeightChoice);
     console.log(howCookedChoice);
 };
+
+
